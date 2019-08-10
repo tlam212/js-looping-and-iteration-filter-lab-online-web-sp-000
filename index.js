c1c82828316cd166782c057be1fd937a3bac12ba
Toni L <tlam212@gmail.com>
2019-08-10 19:50:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution in this file
+function findMatching(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.toLowerCase() === name.toLowerCase();
+  })
+}
+
+function fuzzyMatch (drivers, letter) {
+  let lengthOfName = letter.length;
+  return drivers.filter(function (driver) {
+    return driver.slice(0, lengthOfName) === letter;
+  });
+}
+
+function matchName(drivers, name){
+  return drivers.filter(function(driver){
+    return driver.name.toLowerCase()=== name.toLowerCase()
+  })
+}
